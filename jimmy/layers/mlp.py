6a8ec5a2342ad7cadba293e882932f97f7bf8594
clementpoiret <poiret.clement@outlook.fr>
2024-06-27 12:17:38 +0200

    feat: mlp

---
+++ jimmy/layers/mlp.py
@@ -0,0 +1,30 @@
+from flax import nnx
+
+
+class Mlp(nnx.Module):
+    """Simple MLP for Vision Transformers."""
+
+    def __init__(
+        self,
+        rngs: nnx.Rngs,
+        in_features: int,
+        hidden_features: int,
+        out_features: int,
+        act_layer: nnx.Module = nnx.gelu,
+        dropout_rate: float = 0.0,
+        bias: bool = True,
+    ):
+        self.fc1 = nnx.Linear(in_features, hidden_features, rngs=rngs)
+        self.act = act_layer
+        self.drop1 = nnx.Dropout(dropout_rate, rngs=rngs)
+        self.fc2 = nnx.Linear(hidden_features, out_features, rngs=rngs)
+        self.drop2 = nnx.Dropout(dropout_rate, rngs=rngs)
+
+    def __call__(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+
+        return x
