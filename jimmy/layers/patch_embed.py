510842643d4a820c0fdfb8999f9ac35309be0912
clementpoiret <poiret.clement@outlook.fr>
2024-06-27 14:54:09 +0200

    feat: patch embedding

---
+++ jimmy/layers/patch_embed.py
@@ -0,0 +1,56 @@
+from typing import Optional
+
+import jax.numpy as jnp
+from flax import nnx
+
+
+class PatchEmbed(nnx.Module):
+    """Image to Patch Embedding"""
+
+    def __init__(
+        self,
+        rngs: nnx.Rngs,
+        patch_size: int = 16,
+        in_channels: int = 3,
+        embed_dim: int = 768,
+        norm_layer: Optional[nnx.Module] = None,
+        flatten: bool = True,
+        use_bias: bool = True,
+    ):
+        self.flatten = flatten
+        self.embed_dim = embed_dim
+        self.patch_H, self.patch_W = self.patch_size, self.patch_size
+
+        self.proj = nnx.Conv(
+            in_features=in_channels,
+            out_features=embed_dim,
+            kernel_size=(self.patch_H, self.patch_W),
+            strides=(self.patch_H, self.patch_W),
+            padding="VALID",
+            use_bias=use_bias,
+            rngs=rngs,
+        )
+
+        if norm_layer is not None:
+            self.norm = norm_layer(num_features=embed_dim, rngs=rngs)
+        else:
+            self.norm = None
+
+    def __call__(self, x):
+        _, H, W, C = x.shape
+        assert (
+            H % self.patch_H == 0 and W % self.patch_W == 0
+        ), f"Image size ({H}*{W}) cannot be evenly divided by patch size ({self.patch_H}*{self.patch_W})."
+
+        x = self.proj(x)
+
+        _, H, W, _ = x.shape
+        x = jnp.reshape(x, (x.shape[0], -1, x.shape[-1]))
+
+        if self.norm:
+            x = self.norm(x)
+
+        if self.flatten:
+            x = jnp.reshape(x, (-1, H, W, self.embed_dim))
+
+        return x
