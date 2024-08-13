9600eb4e12d971948f40a8c5f03636aff2fe2b2f
clementpoiret <poiret.clement@outlook.fr>
2024-08-13 16:28:47 +0200

    feat: layer sharing wrapper

--- jimmy/layers/sharing.py
+++ jimmy/layers/sharing.py
@@ -1,5 +1,6 @@
 import jax.numpy as jnp
 from flax import nnx
+from jax.lax import fori_loop, switch
 
 
 class LoRA(nnx.Module):
@@ -43,3 +44,70 @@ class LoRA(nnx.Module):
         x = self.B(self.A(x))
 
         return x * self.scaling
+
+
+class LayerSharing(nnx.Module):
+    """
+    Layer Sharing wrapper responsible for repeating a Callable,
+    while learning LoRA params. This is similar to what is done in MobileLLL [1]
+    and Zamba2 [2]. It allows to use more FLOPs without having to store more params.
+
+    Depending on the position of this wrapper (block-level or layer-level), given three
+    callables A, B and C, you can produce those to kind of repetition patterns:
+    - ABCABC,
+    - AABBCC.
+
+    Note:
+        Repeating a layer twice requires it to produce outputs of the same shape as its inputs
+
+    References:
+        [1] Liu, et al., MobileLLM: Optimizing Sub-billion Parameter Language Models for On-Device Use Cases (2024).
+        [2] https://github.com/Zyphra/Zamba2
+    """
+
+    lora: bool = True
+    repetitions: int = 2
+    lora_kwargs = {"rank": 8, "alpha": 16, "drop_rate": 0.0}
+
+    def __init__(
+        self,
+        dim: int,
+        f: nnx.Module,
+        *,
+        rngs: nnx.Rngs,
+        **kwargs,
+    ):
+        self.__dict__.update(**kwargs)
+
+        self.loras = (
+            [
+                LoRA(
+                    in_features=dim,
+                    out_features=dim,
+                    rngs=rngs,
+                    **self.lora_kwargs,
+                )
+                for i in range(self.repetitions)
+            ]
+            if self.lora
+            else None
+        )
+
+        self.f = f
+
+    def forward(self, i: int, x: jnp.ndarray):
+        if self.loras is None:
+            return self.f(x)
+
+        def apply_lora(j, x):
+            return self.loras[j](x)
+
+        lora_output = switch(
+            i, [lambda x: apply_lora(j, x) for j in range(self.repetitions)], x
+        )
+        return self.f(x) + lora_output
+
+    def __call__(self, x: jnp.ndarray):
+        x = fori_loop(0, self.repetitions, self.forward, x)
+
+        return x
